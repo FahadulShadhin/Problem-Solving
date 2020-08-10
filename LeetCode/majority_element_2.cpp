c5866e4c87b120820917f2a47245340be52e1edb
Fahadul Islam Shadhin <fahadulshadhin17@gmail.com>
2020-08-10 14:08:35 +0600

    Update majority_element_2.cpp

--- LeetCode/majority_element_2.cpp
+++ LeetCode/majority_element_2.cpp
@@ -16,3 +16,43 @@ public:
         return ans;
     }
 };
+
+
+
+// O(1) space
+// Moore voting algorithm
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int candidate1 = 0, candidate2 = 0;
+        int cnt1 = 0, cnt2 = 0;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == candidate1) cnt1++;
+            else if(nums[i] == candidate2) cnt2++;
+            else if(cnt1 == 0) {
+                cnt1++;
+                candidate1 = nums[i];
+            }
+            else if(cnt2 == 0) {
+                cnt2++;
+                candidate2 = nums[i];
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        int count1 = 0, count2 = 0;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == candidate1) count1++;
+            else if(nums[i] == candidate2) count2++;
+        }
+        
+        vector<int> ans;
+        if(count1 > floor(n/3)) ans.push_back(candidate1);
+        if(count2 > floor(n/3)) ans.push_back(candidate2);
+        
+        return ans;
+    }
+};
