6ace59e749df17079d8b83a39ab167578c6c3f54
Shadhin <fahadulshadhin17@gmail.com>
2021-10-26 09:55:44 +0600

    Add files via upload

---
+++ LeetCode/Missing_Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        for i in range(0, n+1):
+            if i not in nums:
+                return i
