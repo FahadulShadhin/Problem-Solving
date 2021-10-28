00cdd4f7facef6dfd416afe7be2886e5c7f2621b
Shadhin <fahadulshadhin17@gmail.com>
2021-10-28 08:39:37 +0600

    Update implement_strStr.py

--- LeetCode/implement_strStr.py
+++ LeetCode/implement_strStr.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/implement-strstr/submissions/
+# https://leetcode.com/problems/implement-strstr/
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
