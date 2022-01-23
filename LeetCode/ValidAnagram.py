5aa8ac3285e7b62551682463614cc83dfe39f4c8
FahadulShadhin <fahadulshadhin17@gmail.com>
2022-01-23 16:14:18 +0100

    Valid Anagram solution 2 added

--- LeetCode/ValidAnagram.py
+++ LeetCode/ValidAnagram.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/valid-anagram/
 
+"""Solution: 1"""
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -8,4 +9,31 @@ class Solution:
         if sorted(s) != sorted(t):
             return False
             
+        return True
+
+
+
+"""Solution: 2"""
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_freq = {}
+        for c in s:
+            if c in s_freq:
+                s_freq[c] += 1
+            else:
+                s_freq[c] = 1
+                
+        t_freq = {}
+        for c in t:
+            if c in t_freq:
+                t_freq[c] += 1
+            else:
+                t_freq[c] = 1
+                
+        if s_freq != t_freq:
+            return False
+        
         return True
