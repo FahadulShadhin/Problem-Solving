a16e6a17f19a1e30b3aa42fb94020f7e757e0eaf
FahadulShadhin <fahadulshadhin17@gmail.com>
2022-01-23 15:52:18 +0100

    valid Anagram solve added

---
+++ LeetCode/ValidAnagram.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/valid-anagram/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        if sorted(s) != sorted(t):
+            return False
+            
+        return True
