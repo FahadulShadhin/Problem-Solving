2d7bafeb305324578d1f15a21ac9af2cd6980f14
FahadulShadhin <fahadulshadhin17@gmail.com>
2022-08-14 19:45:12 +0600

    2 sum example added

--- LeetCode/2-sum.py
+++ LeetCode/2-sum.py
@@ -13,3 +13,21 @@ class Solution:
                 m[val] = i
                 
         return res
+
+"""Example:
+    nums: 2, 7, 11, 15
+    target: 9
+    
+    i = 0:
+        val = 2
+        9 - 2 = 7 not in m:
+            m[2] = 0
+            
+    i = 1:
+        val = 7
+        9 - 7 = 2 is in m:
+            res.append(m[2]) = res.append(0)
+            res.append(1)
+            
+    return [0, 1]
+"""
