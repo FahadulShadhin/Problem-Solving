a902039a882770d44b804a3bf1dba08fe6b017e8
FahadulShadhin <fahadulshadhin17@gmail.com>
2022-12-04 18:59:37 +0600

    80-Remove-Duplicates-from-Sorted-Array-II.py

--- LeetCode/topic-wise-prep/two-pointers-and-sliding-window/80-Remove-Duplicates-from-Sorted-Array-II.py
+++ LeetCode/topic-wise-prep/two-pointers-and-sliding-window/80-Remove-Duplicates-from-Sorted-Array-II.py
@@ -4,17 +4,17 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         """
         0 0 1 1 1 1 2 3 3
-            s | | | | | |    nums[s-2] != nums[f]; swap(nums[s], nums[f]); s+=1, f+=1
+            s | | | | | |     nums[s-2] != nums[f]; swap(nums[s], nums[f]); s+=1, f+=1
             f | | | | | |
-              s | | | | |    nums[s-2] != nums[f]; swap(nums[s], nums[f]); s+=1, f+=1
+              s | | | | |     nums[s-2] != nums[f]; swap(nums[s], nums[f]); s+=1, f+=1
               f | | | | |
-                s | | | |    nums[s-2] == nums[f]; f+=1
+                s | | | |     nums[s-2] == nums[f]; f+=1
                 f | | | |
-                s f | | |    nums[s-2] == nums[f]; f+=1
-                s | f | |    nums[s-2] != nums[f]; swap(nums[s], nums[f]); s+=1, f+=1
-                  s   f |    nums[s-2] != nums[f]; swap(nums[s], nums[f]); s+=1, f+=1
-                    s   f    nums[s-2] != nums[f]; swap(nums[s], nums[f]); s+=1, f+=1
-                      s  f   f == len(nums)-1; return s
+                s f | | |     nums[s-2] == nums[f]; f+=1
+                s | f | |     nums[s-2] != nums[f]; swap(nums[s], nums[f]); s+=1, f+=1
+                  s   f |     nums[s-2] != nums[f]; swap(nums[s], nums[f]); s+=1, f+=1
+                    s   f     nums[s-2] != nums[f]; swap(nums[s], nums[f]); s+=1, f+=1
+                      s   f   f == len(nums)-1; return s
         """
         
         s, f = 2, 2
