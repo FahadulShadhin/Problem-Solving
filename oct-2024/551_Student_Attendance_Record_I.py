d8ef520a506c445a41df9f5991169c00af5aaf62
FahadulShadhin <fahadulshadhin17@gmail.com>
2024-10-30 18:03:38 +0600

    leetcode 551

---
+++ oct-2024/551_Student_Attendance_Record_I.py
@@ -0,0 +1,17 @@
+# https: // leetcode.com/problems/student-attendance-record-i/description/
+
+class Solution:
+  def checkRecord(self, s: str) -> bool:
+    countAbsent = 0
+    for i in range(0, len(s)):
+      if s[i] == 'A':
+          countAbsent += 1
+      if s[i] == 'L' and (i >= 1 and i <= len(s)-2):
+          print(i)
+          if (s[i-1] == 'L') and (s[i+1] == 'L'):
+              return False
+
+    if countAbsent >= 2:
+      return False
+
+    return True
