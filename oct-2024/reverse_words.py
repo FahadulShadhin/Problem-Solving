b97525ad62a668c49aacdab86dee9fc084c10550
FahadulShadhin <fahadulshadhin17@gmail.com>
2024-11-01 13:20:52 +0600

    reverse words of a sentence

---
+++ oct-2024/reverse_words.py
@@ -0,0 +1,33 @@
+# https: // leetcode.com/problems/reverse-words-in -a-string/description/
+
+def reverse_words(s: str) -> str:
+  i, j = 0, len(s) - 1
+  
+  ## skip the white spaces in fron and back ##
+  while s[i] == ' ':
+    i += 1
+  
+  while s[j] == ' ':
+    j -= 1
+  ###########################################
+
+  temp = ''
+  res = []
+  for k in range(i, j+1):
+    if s[k] == ' ':
+      if temp != '':
+        res.append(temp)
+        temp = ''
+    else:
+      temp += s[k]
+
+  res.append(temp)
+      
+  return ' '.join(res[::-1])
+
+if __name__=='__main__':
+  s = "the sky is blue"
+  s1 = "  hello world  "
+  s2 = "a good   example"
+  res = reverse_words(s2)
+  print(res)
