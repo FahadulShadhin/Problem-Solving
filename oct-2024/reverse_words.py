b0498f3c953e82ac82e2ce3c40f3f23b58de867b
FahadulShadhin <fahadulshadhin17@gmail.com>
2024-11-01 13:27:13 +0600

    add a second solution to reverse words of sentence

--- oct-2024/reverse_words.py
+++ oct-2024/reverse_words.py
@@ -25,9 +25,21 @@ def reverse_words(s: str) -> str:
       
   return ' '.join(res[::-1])
 
+def reverse_words_1(s: str) -> str:
+  splitS = s.split(' ')
+  
+  rev = ''
+  for i in range(len(splitS)-1, -1, -1):
+    if splitS[i] != '':
+      rev += splitS[i] + ' '
+  
+  return rev
+
+
 if __name__=='__main__':
   s = "the sky is blue"
   s1 = "  hello world  "
   s2 = "a good   example"
-  res = reverse_words(s2)
+  # res = reverse_words(s2)
+  res = reverse_words_1(s1)
   print(res)
